722582919f31ce04c3d925db709d98171ed05af6
Gary Powell <110321365@umail.ucc.ie>
2013-02-23 12:29:57 +0000

    Initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TeamProject::Application.config.secret_token = '55d89f691d70e929fbae7904e2a6b1e5bce82a99bb65526b25b889a9f985db3ca761083cfa21234d8bef10a8aab3719033472228389a2d55cb87f54fece47ab1'
